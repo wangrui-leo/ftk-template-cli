98bd475bc697422d16ff9024f2f5c7102598f603
wangrui <wangrui@fantaike.ai>
2020-08-26 11:17:58 +0800

    TEMPLATE_GIT_REPO 修改

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-exports.TEMPLATE_GIT_REPO = 'direct:https://github.com/jackie-gan/vue-spa-template.git';
+exports.TEMPLATE_GIT_REPO = 'direct:git@10.10.8.201:trd/react-front-template.git';
 exports.INJECT_FILES = ['package.json'];
